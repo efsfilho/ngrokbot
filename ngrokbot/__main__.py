104d6e76b281c7801491a932bd55e029607c481c
efsfilho <efdsfilho@gmail.com>
2019-11-06 21:38:25 -0300

    user filter added

--- ngrokbot/__main__.py
+++ ngrokbot/__main__.py
@@ -1,26 +1,40 @@
 import os
 import sys
-import pathlib
 import logging
 from bot import bot
 from ngrok import cli
-
-log_dir = './log'
-if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
-
-log_file = os.path.join(log_dir, 'log.log')
+from pathlib import Path
+from dotenv import load_dotenv
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(filename)s > %(funcName)s: %(message)s')
 
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(filename)s > %(funcName)s: %(message)s')
-handler = logging.FileHandler(log_file)
-handler.setFormatter(formatter)
+def config_log():
+    # Config log
+    log_dir = './log'
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
 
-logging.getLogger().addHandler(handler)
-logging.root.setLevel(logging.INFO)
+    log_file = os.path.join(log_dir, 'log.log')
+
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(filename)s > %(funcName)s: %(message)s')
+    handler = logging.FileHandler(log_file)
+    handler.setFormatter(formatter)
+
+    logging.getLogger().addHandler(handler)
+    logging.root.setLevel(logging.INFO)
+
+def main():
+    # Telegram token
+    env_path = Path('.')/'.env'
+    load_dotenv(dotenv_path=env_path)
+
+    telegram_token = os.getenv('TELEGRAM_TOKEN')
+    if telegram_token is None:
+        logging.error('Telegram token not found')
+        exit()
+    bot.run(telegram_token)
 
 if __name__ == '__main__':
-    token = os.environ['BOT1']
-    bot.run(token)
+    config_log()
+    main()
     # cli()
--- ngrokbot/bot/bot.py
+++ ngrokbot/bot/bot.py
@@ -1,14 +1,33 @@
+import os
 import re
 import json
 import logging
 import telegram
+from pathlib import Path
+from functools import wraps
 from ngrok import NgrokManager
+from dotenv import load_dotenv, set_key
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 logger = logging.getLogger()
 
 ngrok = NgrokManager()
 
+env_path = Path('.')/'.env'
+load_dotenv(dotenv_path=env_path)
+user_list = []
+
+def restricted(func):
+    @wraps(func)
+    def wrapped(update, context, *args, **kwargs):
+        user = update.effective_user
+        if user.id not in user_list:
+            logging.info('Unauthorized access denied for User:{}.'.format(user))
+            return
+        return func(update, context, *args, **kwargs)
+    return wrapped
+
+@restricted
 def start(update, context):
     msg = ' '
     try:
@@ -26,6 +45,7 @@ def start(update, context):
     finally:
         update.message.reply_text(msg, parse_mode=telegram.ParseMode.MARKDOWN)
 
+@restricted
 def info(update, context, full=True):
     if ngrok.started():
         try:
@@ -39,23 +59,31 @@ def info(update, context, full=True):
     else:
         update.message.reply_text('ngrok is not running, start it first with /ngrok')
 
+@restricted
 def url(update, context):
     info(update, context, False)
 
+@restricted
 def stop(update, context):
     ngrok.stop()
     update.message.reply_text('signal to stop sent')
-    
+
 def echo(update, context):
     update.message.reply_text(f'echo {update.message.text}')
-
 
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Error "%s"', context.error)
 
+# def update_users():
+#     import random
+#     r = random.randint(0, 9999)
+#     print(r)
+#     set_key(dotenv_path=env_path, key_to_set='TEST', value_to_set=str(r))
+
 def run(token=None):
     ngrok.get_ngrok()
+
     updater = Updater(token, use_context=True)
     disp = updater.dispatcher
 
@@ -65,8 +93,11 @@ def run(token=None):
     disp.add_handler(CommandHandler('stop', stop))
     disp.add_handler(CommandHandler('echo', echo))
     disp.add_handler(MessageHandler(Filters.text, echo))
-
     disp.add_error_handler(error)
+
     updater.start_polling()
     logger.info('Bot started')
+    user_id = os.getenv('USER_ID')
+    if user_id is None:
+        logging.warn('User id not found, no one will be able to use the bot')
     updater.idle()
