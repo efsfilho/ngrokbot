4d712d90b1cecedc544225bc9f7ba9ac23b67e3f
efsfilho <efdsfilho@gmail.com>
2022-10-10 11:00:11 -0300

    requirements uptade

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='ngrokbot',
+   version='0.1.0',
+   description='Ngrok bot manager',
+   author='Eduardo Filho',
+   author_email='efdsfilho@gmail.com',
+   packages=['ngrokbot'],
+#   install_requires=['python-telegram-bot'],
+)
